cff16695f195fe91b028066b0c4961bcdb7178ec
Youssef Briki <youssef.briki@umontreal.ca>
2025-04-06 00:03:17 -0400

    Corrected data_aug script

--- stock_predictor_sentiment_analysis/dora_fine-tuning/data_augmentation.py
+++ stock_predictor_sentiment_analysis/dora_fine-tuning/data_augmentation.py
@@ -2,13 +2,18 @@ from langchain_ollama import OllamaLLM
 from langchain_core.prompts import PromptTemplate
 import argparse
 from deepseek_output_parser import ParaphraseOutputParser
+import httpx
+
 
 #!iconv -f latin1 -t utf8 Sentences_AllAgree.txt -o Sentences_AllAgree_utf8.txt
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--path', required=True, type=str)
+parser.add_argument('--skip', required=False, type=int, default=0)
 args = parser.parse_args()
 path = args.path.split('/')[:-1]
 path = '/'.join(path) + '/'
+skip = args.skip	
 # path = '/home/m/mehrad/brikiyou/scratch/ift6289/IFT6289-project/data/FinancialPhraseBank-v1.0/'
 print(path)
 file_name = args.path.split('/')[-1]
@@ -28,12 +33,12 @@ Paraphrased sentence:
 """,
 )
 
-chain = prompt | llm.with_retry(retry_if_exception_type=(ValueError,), wait_exponential_jitter=False,stop_after_attempt=3) | parser
+chain = prompt | llm.with_retry(retry_if_exception_type=(ValueError, httpx.RemoteProtocolError), wait_exponential_jitter=False,stop_after_attempt=3) | parser
 
 with open(f"{path}{file_name}", 'r') as f:
     lines = f.readlines()
     
-for line in lines:
+for line in lines[skip:]:
     sentence,sentiment = line.split('@')
     sentiment = sentiment.strip()
     for count in range(3):
