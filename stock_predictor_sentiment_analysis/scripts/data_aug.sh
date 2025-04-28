d4d60326b6b1d3196e2bf486f94ef102ebf8822c
Youssef Briki <youssef.briki@umontreal.ca>
2025-04-28 14:45:39 -0400

    halfway done - runnable workflow

--- stock_predictor_sentiment_analysis/scripts/data_aug.sh
+++ stock_predictor_sentiment_analysis/scripts/data_aug.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=DataAug75Agree
+#SBATCH --job-name=DataAugAllAgree
 #SBATCH --nodes=1
 #SBATCH --time=72:00:00
 #SBATCH --gpus-per-node=1
@@ -12,4 +12,4 @@ module load BalamEnv
 source /home/m/mehrad/brikiyou/scratch/to_run.sh
 ollama serve > /home/m/mehrad/brikiyou/scratch/ollama.log 2>&1 &
 source /home/m/mehrad/brikiyou/scratch/ift6289/IFT6289-project/project_venv/bin/activate
-python3 /home/m/mehrad/brikiyou/scratch/ift6289/IFT6289-project/stock_predictor_sentiment_analysis/dora_fine-tuning/data_augmentation.py --path /home/m/mehrad/brikiyou/scratch/ift6289/IFT6289-project/data/FinancialPhraseBank-v1.0/Sentences_75Agree_utf8.txt --skip 3453
+python3 /home/m/mehrad/brikiyou/scratch/ift6289/IFT6289-project/stock_predictor_sentiment_analysis/dora_fine-tuning/data_augmentation.py --path /home/m/mehrad/brikiyou/scratch/ift6289/IFT6289-project/data/FinancialPhraseBank-v1.0/Sentences_AllAgree_utf8.txt --skip 0
