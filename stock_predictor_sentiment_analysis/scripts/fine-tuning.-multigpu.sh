b837080817b0c163b261acd9d2be86d04a039ebd
Youssef Briki <youssef.briki@umontreal.ca>
2025-04-29 16:54:22 -0400

    log included

--- stock_predictor_sentiment_analysis/scripts/fine-tuning.-multigpu.sh
+++ stock_predictor_sentiment_analysis/scripts/fine-tuning.-multigpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=fine-tuning
+#SBATCH --job-name=fine-tuning/1.5b
 #SBATCH --nodes=1
-#SBATCH --time=72:00:00
+#SBATCH --time=12:00:00
 #SBATCH --gpus-per-node=4
 #SBATCH -p compute_full_node
 #SBATCH --output=/home/m/mehrad/brikiyou/scratch/ift6289/IFT6289-project/stock_predictor_sentiment_analysis/scripts/out/slurm-%j.out
@@ -17,6 +17,6 @@ module load pytorch/2.1.2
 module load gcc/12.3.0
 module load cuda/12.3.1
 cd /home/m/mehrad/brikiyou/scratch/ift6289/IFT6289-project/
-#source project_venv_2/bin/activate
+source project_venv_2/bin/activate
 cd stock_predictor_sentiment_analysis/dora_fine-tuning/
-python3 training.py
+python3 training.py > output_1.5b.log
